db40f14ecadfc797acc18c4e1ece034eee6696cc
Aqib Shah <aqib.shah@student.manchester.ac.uk>
2024-05-06 17:05:27 +0100

    Updates to javadoc

--- ConnectFour/src/Game.java
+++ ConnectFour/src/Game.java
@@ -1,14 +1,23 @@
-/**
- * 
- */
 
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+import java.util.Scanner;
 /**
+ * This class is a model of the game ConnectFour.
+ * 
+ * This is my submission for the Ovarro Software Engineer 
+ * Technical Test for the 2024 Graduate Software Engineer
+ * position.
+ * 
+ * 
  * @author ashah
+ * @version 1.0
+ * @since 2024-05-06
  *
  */
-import java.util.logging.Logger;
-import java.util.Scanner;
-
 public class Game {
     private char[][] grid;
     private int width;
@@ -17,8 +26,25 @@ public class Game {
     private int winningRowLength;
     private char currentPlayer;
     private static final Logger logger = Logger.getLogger(Game.class.getName());
+    static {
+        try {
+            // Remove the default console handler
+            Logger logger = Logger.getLogger("");
+            Handler[] handlers = logger.getHandlers();
+            for (Handler handler : handlers) {
+                if (handler instanceof ConsoleHandler) {
+                    logger.removeHandler(handler);
+                }
+            }
 
-
+            // Create a FileHandler that writes log records to a file called "game_logs.txt"
+            Handler fileHandler = new FileHandler("GameLog.txt");
+            fileHandler.setFormatter(new SimpleFormatter()); // You can set a custom formatter if needed
+            logger.addHandler(fileHandler);
+        } catch (Exception e) {
+            logger.severe("Failed to set up logging");
+        }
+    }
     /**
      * Constructor to initialise a new game with custom grid size and winning row length.
      * 
@@ -29,15 +55,17 @@ public class Game {
     public Game(int width, int height, int winningRowLength) {
         if (winningRowLength <= 1)
         {
-        	logger.severe("winning row length must be greater than 1");
-        	logger.info("Setting winning row length to 4");
+        	logger.warning("winning row length must be greater than 1" + System.lineSeparator());
+        	logger.info("Setting winning row length to 4" + System.lineSeparator());
+        	System.out.println("Using default winning row length (4)");
         	winningRowLength = 4;
         	
         }
         if (width < winningRowLength || height < winningRowLength) {
-        	logger.severe("Grid size must be bigger than the winning row length");
+        	logger.warning("Grid size must be bigger than the winning row length" + System.lineSeparator());
         	// standard connect four grid
-        	logger.info("Setting 7x6 grid");
+        	logger.info("Setting 7x6 grid" + System.lineSeparator());
+        	System.out.println("Using default grid size (7x6)");
         	width = 7;
         	height = 6;
         }
@@ -48,14 +76,15 @@ public class Game {
         this.gridSize[1] = height;
         this.winningRowLength = winningRowLength;
         initializeGrid();
-        currentPlayer = 'R'; // Red player starts
+        currentPlayer = 'R';
+        // Red player starts
     }
 
     /**
-     * Method to initialise the grid that will be used for the game.
-     * 
-     */
+     * Method to initialise the game grid
+    */
     private void initializeGrid() {
+    	logger.info("Initialising grid" + System.lineSeparator());
         this.grid = new char[this.height][this.width];
         // Initialise grid with empty cells
         for (int i = 0; i < this.height; i++) {
@@ -63,7 +92,6 @@ public class Game {
                 this.grid[i][j] = '.';
             }
         }
-        //printGrid();
     }
     
     
@@ -71,9 +99,10 @@ public class Game {
      * Method to get the size of the grid
      * 
      * @return gridSize         The grid size is an array containing the width and height
-     */
+    */
     public int[] getGridSize()
     {
+    	logger.info("Getting grid size " + System.lineSeparator());
     	return this.gridSize;
     }
     
@@ -84,6 +113,7 @@ public class Game {
     */
 	public char[][] getGrid()
 	{
+		logger.info("Getting grid " + System.lineSeparator());
 		return this.grid;
 	}
     
@@ -94,6 +124,7 @@ public class Game {
     */
     public int getWinningRowLength()
     {
+    	logger.info("Getting length of a winning row" + System.lineSeparator());
     	return this.winningRowLength;
     }
     
@@ -104,21 +135,26 @@ public class Game {
      * @return true if the token was successfully added, false otherwise.
      */
     public boolean addToken(int column) {
+    	logger.info("Attempting to insert token" + System.lineSeparator());
     	if (column > this.width - 1 || column < 0)
     	{
-    		logger.warning("Column is not on grid");
+    		// Logging to file
+    		logger.warning("Column is not on grid" + System.lineSeparator());
+    		// Logging to console with System.out.println
+    		System.out.println("Column cannot be found on the grid");
     		return false;
     	}
     	for (int i = this.height - 1; i >= 0; i--) {
             if (this.grid[i][column] == '.')
             {
-            	logger.info("Inserting token");
+            	logger.info("Inserting token " + System.lineSeparator());
             	this.grid[i][column] = this.currentPlayer;
             	this.currentPlayer = this.getNextPlayer();
             	return true;
             }
         }
-    	logger.info("Cannot find space to insert token");
+    	logger.info("Selected column is full" + System.lineSeparator());
+    	System.out.println("Selected column is full, please try another column");
         return false;
     }
 
@@ -126,6 +162,8 @@ public class Game {
      * Method to show the game grid state/progress.
     */
     public void showGridState() {
+    	logger.info("Printing grid" + System.lineSeparator());
+    	System.out.println();
     	for (int i = 0; i < this.width; i++)
     	{
     		System.out.print(i);
@@ -138,8 +176,7 @@ public class Game {
                 System.out.print(" ");
             }
         }
-    	System.out.println();
-    	System.out.println("Player to move: " + this.getCurrentPlayer());
+    	System.out.println();    	
     }
 
     /**
@@ -148,6 +185,7 @@ public class Game {
      * @return The next player (either 'R' or 'Y').
      */
     public char getNextPlayer() {
+    	logger.info("Retrieving next player" + System.lineSeparator());
     	if (this.currentPlayer == 'R')
     	{
     		return 'Y';
@@ -161,6 +199,7 @@ public class Game {
      * @return The current player (either 'R' or 'Y').
     */
     public char getCurrentPlayer() {
+    	logger.info("Retrieving current player" + System.lineSeparator());
     	return this.currentPlayer;
     }
 
@@ -168,10 +207,11 @@ public class Game {
 	 * Method to check if there is a winner.
 	 * 
 	 * @param board The board that is being checked
-	 * @winningRowLength The winning row length associated with the board
+	 * @param winningRowLength The winning row length associated with the board
 	 * @return The winner player ('R' or 'Y') if there is a winner, '.' if no winner yet.
 	*/
     public char checkWinner(char[][] board, int winningRowLength) {
+    	logger.info("Checking winner" + System.lineSeparator());
         // Check rows
         for (int row = 0; row < board.length; row++) {
             for (int col = 0; col <= board[row].length - winningRowLength; col++) {
@@ -183,7 +223,13 @@ public class Game {
                         break;
                     }
                 }
-                if (rowWin) return token;
+                if (rowWin)
+                {
+                	logger.info("Winner found in rows" + System.lineSeparator());
+                	return token;
+                	
+                }
+                	
             }
         }
 
@@ -198,7 +244,11 @@ public class Game {
                         break;
                     }
                 }
-                if (colWin) return token;
+                if (colWin)
+                {
+                	logger.info("Winner found in columns" + System.lineSeparator());
+                	return token;
+                }
             }
         }
 
@@ -213,7 +263,11 @@ public class Game {
                         break;
                     }
                 }
-                if (diagonalWin) return token;
+                if (diagonalWin)
+                {
+                	logger.info("Winner found from top left to bottom right" + System.lineSeparator());
+                	return token;
+                }
             }
         }
 
@@ -228,11 +282,17 @@ public class Game {
                         break;
                     }
                 }
-                if (diagonalWin) return token;
+                if (diagonalWin)
+                {
+                	logger.info("Winner found from bottom right to top left" + System.lineSeparator());
+                	return token;
+                	
+                }
+                
             }
         }
 
-        // If no winner is found, return '.'
+        logger.info("No winner found" + System.lineSeparator());
         return '.';
     }
     
@@ -243,10 +303,56 @@ public class Game {
     */
     public static void playGame(Game game)
     {
+    	boolean finished = false;
+    	while (!finished)
+    	{
+    		game.showGridState();
+    		boolean valid = false;
+    		while (!valid)
+    		{
+    			try
+            	{
+            		Scanner scan = new Scanner(System.in);
+            		int[] gridSize = game.getGridSize();
+            		System.out.println("Player to move: " + game.getCurrentPlayer());
+            		System.out.println("Please enter column number.");
+                	int col = scan.nextInt();
+                	// checking input here as well, so function returns 
+                	// false in a draw
+                	if (col > gridSize[0] || col < 0)
+                	{
+                		logger.warning("Column is not on grid." + System.lineSeparator());
+                		System.out.println("Column cannot be found on the grid.");
+                		continue;
+                	}
+                	boolean move = game.addToken(col);
+                	if (move) 
+                	{
+                		valid = true;
+                	}
+                	char result = game.checkWinner(game.getGrid(), game.getWinningRowLength());
+                	if (result != '.')
+                	{
+                		logger.info("Winner: " + String.valueOf(result) + System.lineSeparator());
+                		System.out.println("Winner: " + String.valueOf(result));
+                		finished = true;
+                	}
+            	}
+            	catch (Exception e)
+            	{
+            		System.out.println("Invalid input, please enter a valid column number");
+            	}
+    			
+    		}	
+    	}
     	game.showGridState();
     }
-
-    // Main method
+    
+    /**
+     * Main method to accept user input for the parameters required
+     * to run the game. It attempts to start the game after verifying 
+     * the inputs are correct.
+    */
     public static void main(String[] args) {
     	// variable to check if parameters have been set correctly
     	boolean set = false;
@@ -267,7 +373,7 @@ public class Game {
         	}
         	catch(Exception e) 
         	{
-        		System.out.println("Invalid combination, please try again");
+        		System.out.println("Please enter columns, rows and winning row length as numbers");
         	}
     	}
 
